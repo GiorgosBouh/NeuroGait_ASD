6fa95ddd65f9fb569460ee023f7246badc0ed278
Georgios Bouchouras <140192942+GiorgosBouh@users.noreply.github.com>
2025-07-23 12:37:11 +0300

    changes

--- neurogait.py
+++ neurogait.py
@@ -640,10 +640,12 @@ class NeuroGaitMLAnalysis:
         def convert_for_json(obj):
             if isinstance(obj, np.ndarray):
                 return obj.tolist()
-            elif isinstance(obj, np.integer):
+            elif isinstance(obj, (np.integer, np.int64)):
                 return int(obj)
-            elif isinstance(obj, np.floating):
+            elif isinstance(obj, (np.floating, np.float64)):
                 return float(obj)
+            elif isinstance(obj, (np.bool_, bool)):
+                return bool(obj)
             elif isinstance(obj, dict):
                 return {k: convert_for_json(v) for k, v in obj.items()}
             elif isinstance(obj, list):
